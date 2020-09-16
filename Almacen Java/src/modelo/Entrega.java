9d8dc7c8e1a1e68f94fb6a7e2474cc4693a98246
Martín <Martn@DESKTOP-H730QN9>
2020-09-16 02:16:47 -0300

    Que no panda el cunico

---
+++ Almacen Java/src/modelo/Entrega.java
@@ -0,0 +1,46 @@
+package modelo;
+
+import java.time.LocalDate;
+
+public class Entrega {
+	protected int id;
+	protected LocalDate fecha;
+	protected boolean efectivo;
+
+	public Entrega(int id, LocalDate fecha, boolean efectivo) {
+		super();
+		this.id = id;
+		this.fecha = fecha;
+		this.efectivo = efectivo;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
+	public boolean isEfectivo() {
+		return efectivo;
+	}
+
+	public void setEfectivo(boolean efectivo) {
+		this.efectivo = efectivo;
+	}
+
+	@Override
+	public String toString() {
+		return "Entrega [id=" + id + ", fecha=" + fecha + ", efectivo=" + efectivo + "]";
+	}
+
+}
